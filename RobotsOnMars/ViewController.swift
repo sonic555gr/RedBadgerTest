a598870e106890e94de88f70ef18f64a0f25195a
Alkiviadis Papadakis <alkiviadis@laundrapp.com>
2019-01-10 12:43:35 +0000

    Initial Commit

---
+++ RobotsOnMars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RobotsOnMars
+//
+//  Created by Alkiviadis Papadakis on 10/01/2019.
+//  Copyright © 2019 Alkimo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
